fcc55b7fd13fe3e68fc989182a3b04704ad7c099
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2025-04-07 01:14:01 +0800

    change the git plugin to diffview.nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,24 +1,10 @@
 return {
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-			"nvim-telescope/telescope.nvim", -- optional
-		},
-		config = function()
-			require("neogit").setup({
-				kind = "vsplit",
-				integrations = {
-					diffview = true,
-					telescope = true,
-				},
-				commit_editor = {
-					kind = "split",
-				},
-			})
-
-			vim.keymap.set("n", "<Leader>gi", ":Neogit<CR>", { silent = true })
-		end,
-	},
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      vim.keymap.set("n", "<Leader>gi", ":DiffviewOpen<CR>", { silent = true })
+      vim.keymap.set("n", "<Leader>gc", ":DiffviewClose<CR>", { silent = true })
+      vim.keymap.set("n", "<Leader>gh", ":DiffviewFileHistory<CR>", { silent = true })
+    end,
+  },
 }
